c3c81a73fe23cec04c743b0d66b9cd347190b152
jpmalere <jpmalere@gmail.com>
2015-08-23 18:43:34 -0300

    commit

--- server.R
+++ server.R
@@ -10,9 +10,24 @@ nGram <- list(tdm2gram,tdm3gram,tdm4gram)
 nWords <- 3
 # shinyServer
 shinyServer(function(input, output) {
-    words <- reactive({
-        input$predButton
-        predWord(input$text,nGram,nWords)
+#    words <- reactive({
+#        input$predButton
+    words <- eventReactive(input$predButton,{
+        
+        inputPhrase <- Corpus(VectorSource(input$text),readerControl = list(language="en_US"))
+        inputPhrase <- tm_map(inputPhrase, tolower)
+        inputPhrase <- tm_map(inputPhrase, removePunctuation)
+        inputPhrase <- tm_map(inputPhrase, removeNumbers)
+        inputPhrase <- tm_map(inputPhrase, removeWords, stopwords("english"))
+        inputPhrase <- tm_map(inputPhrase, removeWords, stopwords("SMART"))
+        inputPhrase <- tm_map(inputPhrase, stripWhitespace)
+        inputPhrase <- tm_map(inputPhrase, PlainTextDocument)
+        inputPhrase <- lapply(inputPhrase,as.character)[[1]]
+        aux <- strsplit(inputPhrase," ")
+        inputPhrase <- paste(aux[[1]][(length(aux[[1]])-2):(length(aux[[1]]))],collapse=' ')
+        print(paste("Input:",inputPhrase))
+        
+        predWord(inputPhrase,nGram,nWords)
     })
     output$predWords <- renderText({
         words()
--- ui.R
+++ ui.R
@@ -2,7 +2,21 @@ library(shiny)
 shinyUI(fluidPage(
     titlePanel("Predict words after a given input text"),
     fluidRow(
-        column(8, wellPanel(
+        column(12, wellPanel(
+            p("This data product consists of three elements. An input text box, a 'Predict' button and a text space where three words on descending order of scores are presented separated by ';'"),
+            p(""),
+            p("After the input text is inserted and the 'Predict' button is pressed, three words with the highest scores appear after a few seconds;"),
+            p(""),
+            p("If no text is available on the input text box or the last words are not available on the term-document matrix, the word 'the' appears as the result."),
+            p(""),
+            p("More details about the characteristics and limitations of this data product can be found on the links below:"),
+            p("Github:"),
+            a(href="https://github.com/jpmalere/capstoneProject","https://github.com/jpmalere/capstoneProject"),
+            p(""),
+            p("Rpubs:"),
+            a(href="http://rpubs.com/jpmalere/capstoneProject","http://rpubs.com/jpmalere/capstoneProject"),
+            p(""), 
+            p(""),
             textInput("text", "input text:", "input text"),
             
             br(),
